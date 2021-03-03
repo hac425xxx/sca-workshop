28bc4c9e0102e9f0d624930482798ee1ba8b4461
hac425 <hac425xxx@gmail.com>
2021-03-03 07:19:26 -0800

    fix warn

--- fortify-example/build_fortify_fpr.sh
+++ fortify-example/build_fortify_fpr.sh
@@ -1,3 +1,5 @@
 rm -rf libexample.so
 /home/hac425/sca/fortify/bin/sourceanalyzer -b fortify-example make
 /home/hac425/sca/fortify/bin/sourceanalyzer -b fortify-example -scan -f fortify-example.fpr
+
+# /home/hac425/sca/fortify/bin/sourceanalyzer -no-default-rules -rules /home/hac425/sca/decrypt_rules/ -b fortify-example -scan -f fortify-example.fpr
