7fbdf341bfe4833c59637f0a2a8f1f2cbbd7ce37
hac425 <hac425xxx@gmail.com>
2021-03-05 07:55:08 -0800

    add system_rules

--- fortify-example/build_fortify_fpr.sh
+++ fortify-example/build_fortify_fpr.sh
@@ -3,3 +3,6 @@ rm -rf libexample.so
 # /home/hac425/sca/fortify/bin/sourceanalyzer -b fortify-example -scan -f fortify-example.fpr
 
 /home/hac425/sca/fortify/bin/sourceanalyzer -no-default-rules -rules hello.xml -b fortify-example -scan -f fortify-example.fpr
+
+
+# /home/hac425/sca/fortify/bin/sourceanalyzer -no-default-rules -rules hello_array.xml -b fortify-example -scan -f fortify-example.fpr -D com.fortify.sca.MultithreadedAnalysis=false  -Ddebug.dump-structural-tree 2> tree.tree
